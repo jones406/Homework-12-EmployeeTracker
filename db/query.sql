6e3af6ab227380a6c6dee940849693c1fdd8bbe0
brooke <brookejones406@gmail.com>
2021-11-11 15:39:05 -0600

    initial commit

---
+++ db/query.sql
@@ -0,0 +1,2 @@
+-- Functions for performing specific SQL queries: constructor function or class could be helpful for organizing these.
+
---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS books_db;
+CREATE DATABASE books_db;
+
+USE books_db;
+
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
+);
+
+CREATE TABLE role (
+  id INT PRIMARY KEY,
+  title VARCHAR(30)
+  salary DECIMAL
+  department_id: INT
+);
+
+-- role_id ref employee role
+-- manager_id ref to another employee, null if no manager
+CREATE TABLE employee (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(30)
+  last_name VARCHAR(30)
+  role_id INT
+  manager_id: INT
+);
+
